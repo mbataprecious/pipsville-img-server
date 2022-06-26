d1c2864911d3b8ea77b7f85d2241eaa0532c4a6e
mbataprecious <mbataprecious9@gmail.com>
2022-06-26 22:00:19 +0100

    updated the ui

--- middlewares/formidable.mjs
+++ middlewares/formidable.mjs
@@ -4,13 +4,12 @@ const form = formidable({ keepExtensions: true, maxFileSize: 3 * 1024 * 1024 });
 
 export default async function parseMultipartForm(req, res, next) {
   const contentType = req.headers['content-type'];
-  console.log(contentType);
   if (contentType && contentType.indexOf('multipart/form-data') !== -1) {
     form.parse(req, (err, fields, files) => {
       if (!err) {
         req.body = fields; // sets the body field in the request object
         req.files = files; // sets the files field in the request object
-        console.log(files);
+        
       }
       next(); // continues to the next middleware or to the route
     });
--- routes/uploadRoute.mjs
+++ routes/uploadRoute.mjs
@@ -25,7 +25,6 @@ const bucket = proctAdmin().storage().bucket(`gs://pipsvile.appspot.com`);
   try {
     const { userId } = req.params;
     //get picture from files
-    console.log(userId)
     const file = req.files.photo;
 
     const uploadFileFirebase = await uploadFile(file.filepath, file.newFilename);
@@ -34,7 +33,6 @@ const bucket = proctAdmin().storage().bucket(`gs://pipsvile.appspot.com`);
       return response(res, 500, 'error uploading to firebase', null);
     }
     const user=await User.findByIdAndUpdate( userId , { imageUrl: uploadFileFirebase[0].metadata.mediaLink },{new:true});
-    console.log(user)
     return response(res, 200, 'success', uploadFileFirebase[0].metadata.mediaLink);
   } catch (err) {
     console.log(err);
@@ -48,10 +46,14 @@ const bucket = proctAdmin().storage().bucket(`gs://pipsvile.appspot.com`);
     //const file = await re.files.photo;
     const file = req.files.photo;
 
-    console.log(file);
     const uploadFileFirebase = await uploadFile(file.filepath, file.newFilename);
     if (!uploadFileFirebase) {
       return response(res, 400, 'error uploading to firebase', null);
+    }
+    const verified=await User.findById(userId)
+    console.log(verified)
+    if(verified.isVerified){
+      return response(res, 200, 'already verified');
     }
     await User.findByIdAndUpdate( userId , { IdImg: uploadFileFirebase[0].metadata.mediaLink });
 
