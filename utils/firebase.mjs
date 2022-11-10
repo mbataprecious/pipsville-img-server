5ec05a9939db508f4bfebb80436537795b822c77
mbataprecious <mbataprecious9@gmail.com>
2022-11-10 04:49:24 +0100

    prepare for raiway

--- utils/firebase.mjs
+++ utils/firebase.mjs
@@ -1,12 +1,18 @@
 //Firebase Dependencies
 import firebaseAdmin from 'firebase-admin';
 // import serviceAccount from "../config/firebase_key.json"
+//import { readFile } from 'fs/promises';
 
-import { readFile } from 'fs/promises';
 
-const serviceAccount = JSON.parse(await readFile(new URL('../config/firebase_key.json', import.meta.url)));
 
-const initiateFirebase = () => {
+//const serviceAccount = JSON.parse(await readFile(new URL('../config/firebase_key.json', import.meta.url)));
+
+const initiateFirebase = async() => {
+  const res = await fetch('https://f004.backblazeb2.com/file/pipsville-bucket/firebase_key.json');
+  const serviceAccount = await res.json();
+  if (!res.ok) {
+  throw new Error("failed to fetch firebase key")
+}
   try {
     return firebaseAdmin.initializeApp({
       credential: firebaseAdmin.credential.cert(serviceAccount),
